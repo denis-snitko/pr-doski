7e005d7facd81507a22837599cb2e0cf21267eda
Denis Snitko <denissnitko@gmail.com>
2020-05-19 13:53:20 +0300

    Правки формы

--- js/main.js
+++ js/main.js
@@ -164,8 +164,30 @@
 
 var modalWindowOverlay = document.querySelector('.modal-window-overlay');
 var modalWindowClose = document.querySelector('.modal-window__close');
+var labelText = document.querySelector('.form__name');
+var inputText = document.querySelector('.form__row > input');
+var labelTel = document.querySelector('.form__tel');
+var inputTel = document.querySelector('input[type="tel"]');
 modalWindowClose.addEventListener('click', function () {
   modalWindowOverlay.setAttribute('style', 'display: none');
+  labelText.classList.remove('label--active');
+  inputText.value = '';
+  labelTel.classList.remove('label--active');
+  inputTel.value = '';
+});
+labelText.addEventListener('click', function () {
+  labelText.classList.add('label--active');
+  inputText.focus();
+});
+inputText.addEventListener('focus', function () {
+  labelText.classList.add('label--active');
+});
+labelTel.addEventListener('click', function () {
+  labelTel.classList.add('label--active');
+  inputTel.focus();
+});
+inputTel.addEventListener('focus', function () {
+  labelTel.classList.add('label--active');
 });
 
 /***/ }),
@@ -319,47 +341,45 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slick-slider').slick({
 /*!***************************************************!*\
   !*** ./src/blocks/pages/p-contacts/p-contacts.js ***!
   \***************************************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
-/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var slick_carousel__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! slick-carousel */ "./node_modules/slick-carousel/slick/slick.js");
-/* harmony import */ var slick_carousel__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(slick_carousel__WEBPACK_IMPORTED_MODULE_1__);
-
-
-jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider').slick({
-  prevArrow: '<span class="slider-arrow-catalog--left"></span>',
-  nextArrow: '<span class="slider-arrow-catalog--right"></span>',
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: 0,
-  autoplaySpeed: 2000,
-  arrows: true,
-  pauseOnHover: true,
-  responsive: [{
-    breakpoint: 1200,
-    settings: {
-      slidesToShow: 3,
-      slidesToScroll: 1
-    }
-  }, {
-    breakpoint: 900,
-    settings: {
-      slidesToShow: 2,
-      slidesToScroll: 1
-    }
-  }, {
-    breakpoint: 610,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }]
-});
+// import $ from 'jquery';
+// import 'slick-carousel';
+// $('.slider').slick({
+//     prevArrow: '<span class="slider-arrow-catalog--left"></span>',
+//     nextArrow: '<span class="slider-arrow-catalog--right"></span>',
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     autoplay: 0,
+//     autoplaySpeed: 2000,
+//     arrows: true,
+//     pauseOnHover: true,
+//     responsive: [
+//         {
+//             breakpoint: 1200,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//             }
+//         },
+//         {
+//             breakpoint: 900,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 610,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//     ]
+// });
 
 /***/ }),
 
@@ -409,6 +429,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _blocks_pages_p_product_card_p_product_card__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_blocks_pages_p_product_card_p_product_card__WEBPACK_IMPORTED_MODULE_5__);
 /* harmony import */ var _modules_s_gallery_s_gallery__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! %modules%/s-gallery/s-gallery */ "./src/blocks/modules/s-gallery/s-gallery.js");
 /* harmony import */ var _blocks_pages_p_contacts_p_contacts__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../../blocks/pages/p-contacts/p-contacts */ "./src/blocks/pages/p-contacts/p-contacts.js");
+/* harmony import */ var _blocks_pages_p_contacts_p_contacts__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(_blocks_pages_p_contacts_p_contacts__WEBPACK_IMPORTED_MODULE_7__);
 /* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! %modules%/footer/footer */ "./src/blocks/modules/footer/footer.js");
 /* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(_modules_footer_footer__WEBPACK_IMPORTED_MODULE_8__);
 
